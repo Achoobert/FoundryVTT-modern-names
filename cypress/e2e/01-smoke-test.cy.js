d577a686ee249d4ba5810d4e24e58c93c0be6e18
Isaac Schubert <8057443+Achoobert@users.noreply.github.com>
2026-06-25 11:27:02 +0700

    run smoke test first

---
+++ cypress/e2e/01-smoke-test.cy.js
@@ -0,0 +1,14 @@
+describe('Smoke tests', () => {
+  it('should visit the home page', () => {
+    cy.visit('/')
+    cy.get('body').should('exist', { timeout: 10000 })
+  })
+  it('should login', () => {
+    // There may be first-setup screen we have to ignore
+    cy.licenseAgreeAndClickAccept()
+    cy.setupInputPasswordAndClickLogin()
+    cy.closeTourOverlay()
+    cy.loginAsGM()
+    cy.get('body').should('exist', { timeout: 10000 })
+  })
+})
--- cypress/e2e/smoke-test.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Smoke tests', () => {
-  it('should visit the home page', () => {
-    cy.visit('/')
-    cy.get('body').should('exist', { timeout: 10000 })
-  })
-})
